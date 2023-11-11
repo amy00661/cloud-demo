441741c1ddd86f27c86e226e8ec91b7242e8bf20
Amy Chung <amy00661@gmail.com>
2023-11-11 15:25:44 +0800

    【SpringCloud02】2.4.將FeignClient抽取成1個新的module並進行調用

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages="cn.itcast.feign.clients")
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
